cc67f0991b90a02923c0d6feef69fdb90a7e78a3
ayush samor <ayushsamor21@gmail.com>
2025-06-01 13:39:27 +0530

    The first book schema for booksy

--- database_schema/tables_schema/books.sql
+++ database_schema/tables_schema/books.sql
@@ -1,1 +1,14 @@
-books
+CREATE TABLE book_schema(
+book_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(255) NOT NULL,
+publisher_id INT NOT NULL,
+description VARCHAR(255),
+audio_availability BOOLEAN NULL DEFAULT 0,
+price INT NOT NULL,
+publish_date DATETIME NOT NULL,
+created_by INT,
+updated_by INT,
+created_at DATETIME,
+updated_at DATETIME,
+status ENUM('pending', 'active', 'inactive')
+);
