55444bc4a147ee9401ca2a518e171abd69bdfacf
ayush samor <ayushsamor21@gmail.com>
2025-06-01 14:31:30 +0530

    this is the first author schema

--- database_schema/tables_schema/authors.sql
+++ database_schema/tables_schema/authors.sql
@@ -1,1 +1,11 @@
-Hello
+CREATE TABLE authors_schema(
+authors_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(255) NOT NULL,
+description VARCHAR(999),
+email VARCHAR(255) NOT NULL,
+created_by INT,
+updated_by INT,
+created_at DATETIME,
+updated_at DATETIME,
+status ENUM('pending', 'active', 'inactive')
+);
