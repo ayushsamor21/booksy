9409c527dfda826cac9f24c61324eef6607b69cc
rajveer <rajveercharan0749@gmail.com>
2025-06-01 13:12:04 +0530

    category schema

--- database_schema/tables_schema/categories.sql
+++ database_schema/tables_schema/categories.sql
@@ -1,1 +1,11 @@
-book categories
+CREATE TABLE category_schema(
+category_id INT NOT NULL PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+description VARCHAR(999),
+parent_id INT NOT NULL,
+created_by INT,
+updated_by INT,
+created_at DATETIME,
+updated_at DATETIME,
+status ENUM('pending', 'active', 'inactive')
+);
