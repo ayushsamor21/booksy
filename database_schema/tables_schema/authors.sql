77f22005a84405017f1176ba152336d9b7e443cf
Bhawani <bhawani021@gmail.com>
2025-06-03 07:09:04 +0530

    18: code review

--- database_schema/tables_schema/authors.sql
+++ database_schema/tables_schema/authors.sql
@@ -1,11 +1,11 @@
 CREATE TABLE authors_schema(
-authors_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(255) NOT NULL,
-description VARCHAR(255),
-email VARCHAR(255) NOT NULL,
-created_by INT,
-updated_by INT,
-created_at DATETIME,
-updated_at DATETIME,
-status ENUM('pending', 'active', 'inactive')
+    authors_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    email VARCHAR(255) NOT NULL,
+    created_by INT,
+    updated_by INT,
+    created_at DATETIME,
+    updated_at DATETIME,
+    status ENUM('pending', 'active', 'inactive')
 );
--- database_schema/tables_schema/categories.sql
+++ database_schema/tables_schema/categories.sql
@@ -1,11 +1,11 @@
 CREATE TABLE category_schema(
-category_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(255) NOT NULL,
-description VARCHAR(255),
-parent_id INT NOT NULL,
-created_by INT,
-updated_by INT,
-created_at DATETIME,
-updated_at DATETIME,
-status ENUM('pending', 'active', 'inactive')
+    category_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    parent_id INT NOT NULL,
+    created_by INT,
+    updated_by INT,
+    created_at DATETIME,
+    updated_at DATETIME,
+    status ENUM('pending', 'active', 'inactive')
 );
