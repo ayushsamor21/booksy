54cc642f8121c26d318c85390cdf4b1a2c0835e9
ayush samor <ayushsamor21@gmail.com>
2025-07-20 10:10:23 +0530

    Sample API handler

--- restful_api/sampleApiHandler.py
+++ restful_api/sampleApiHandler.py
@@ -1,88 +1,200 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import argparse
+import mysql.connector
 import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from datetime import datetime
 
-authors = []
+from restful_api.db.authors_operation import AuthorOperation
 
-HOST = 'localhost'
-PORT = 8002
+parser = argparse.ArgumentParser(description='API request to MySql Server')
+parser.add_argument('-host', type=str, default='localhost', help='Server Host')
+parser.add_argument('-port', type=int, default=8002, help='Server port')
+parser.add_argument('-username', type=str, help='Your username')
+parser.add_argument('-password', type=str, help='Your password')
+parser.add_argument('-database', type=str, help='Your database')
+args = parser.parse_args()
+
+SERVER_HOST = args.host
+SERVER_PORT = args.port
+
+def convert_datetime(obj):
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+    return obj
+
 
 class SampleApiHandler(BaseHTTPRequestHandler):
 
-    def _set_headers(self, status=200):
+    def author_instance(self):
+        self.author = AuthorOperation(args.host, args.port, args.database, args.username, args.password)
+
+    def _set_headers(self, status = 200):
         self.send_response(status)
-        self.send_header("Content-type", "application/json")
+        self.send_header('Content-Type', 'application/json')
         self.end_headers()
-
-    def do_GET(self):
-        if self.path == "/authors":
-            self._set_headers()
-            output = json.dumps(authors)
-            self.wfile.write(output.encode('utf-8'))
-        else:
-            self._set_headers(404)
-            self.wfile.write(json.dumps({"error": "Not found"}).encode())
-
 
     def _get_request_body(self):
         content_length = int(self.headers.get('Content-Length', 0))
         body = self.rfile.read(content_length)
         return json.loads(body.decode('utf-8')) if body else {}
 
+    def do_GET(self):
+        if self.path == '/authors':
+            authors = self.author.read_authors()
+            self._set_headers()
+            self.wfile.write(json.dumps(authors, default=str).encode('utf-8'))
+        else:
+            self._set_headers(400)
+            self.wfile.write(json.dumps({"error": "Not found"}).encode())
+
     def do_POST(self):
         if self.path == '/authors':
             body = self._get_request_body()
-            author_id = len(authors) + 1
-            author = {
-                'id': author_id,
-                'name': body.get('name', 'Unnamed authors'),
-                'city': body.get('city', 'unknown city')
-            }
-            authors.append(author)
+            name = body.get('name', 'unknown')
+            description = body.get('description', 'No-description')
+            email = body.get('email', 'No-email')
+            author_id = self.author.insert_authors(name, description, email)
             self._set_headers(201)
-            self.wfile.write(json.dumps(author).encode())
+            self.wfile.write(json.dumps({"id": author_id}).encode())
         else:
             self._set_headers(404)
-            self.wfile.write(b'{"error": "Not found"}')
+            self.wfile.write(json.dumps({"error": "Not found"}).encode())
+
 
     def do_PUT(self):
-        author_id = self.path.split('/')[-1]
-        author_exists = False
-        for author in authors:
-            if author['id'] == int(author_id):
-                author_exists = True
-                body = self._get_request_body()
-                body['id'] = author_id
+        path_parts = self.path.strip('/').split('/')
+        if len(path_parts) == 2 and path_parts[0] == "authors":
+            try:
+                author_id = int(path_parts[1])
+            except ValueError:
+                self._set_headers(400)
+                self.wfile.write(b'{"error": "Invalid ID"}')
+                return
 
-                # change in data store
-                author['name'] = body['name']
-                author['city'] = body['city']
-                self._set_headers(200)
-                self.wfile.write(json.dumps(author).encode())
-                break
+            body = self._get_request_body()
+            name = body.get('name', 'unknown')
+            description = body.get('description', 'No-description')
+            email = body.get('email', 'No-email')
+            self.author.update_authors(author_id, name, description, email)
 
-            if not author_exists:
-                self._set_headers(404)
-                self.wfile.write(b'{"error": "Not found"}')
+            self._set_headers(200)
+            self.wfile.write(json.dumps({"message": "Author updated"}).encode())
+        else:
+            self._set_headers(404)
+            self.wfile.write(json.dumps({"error": "Author not found"}).encode())
 
     def do_DELETE(self):
-        author_id = self.path.split('/')[-1]
-        author_exists = False
-        for author in authors:
-            if author['id'] == int(author_id):
-                author_exists = True
-                authors.remove(author)
-                self._set_headers(202)
-                break
-        if not author_exists:
-            self._set_headers(404)
-            self.wfile.write(b'{"error": "Not found"}')
+        path_parts = self.path.strip('/').split('/')
+        if len(path_parts) == 2 and path_parts[0] == "authors":
+            try:
+                author_id = int(path_parts[1])
+            except ValueError:
+                self._set_headers(400)
+                self.wfile.write(b'{"error": "Invalid ID"}')
+                return
 
-def run():
-    server_address = (HOST, PORT)
-    httpd = HTTPServer(server_address, SampleApiHandler)
-    print(f"Starting server on http://{HOST}:{PORT}")
-    httpd.serve_forever()
+            self.author.delete_authors(author_id)
+            self._set_headers(200)
+            self.wfile.write(json.dumps({"message": "Author deleted"}).encode())
+        else:
+            self._set_headers(404)
+            self.wfile.write(json.dumps({"error": "Author not found"}).encode())
+
+
+def main():
+        server_address = (SERVER_HOST, SERVER_PORT)
+        httpd = HTTPServer(server_address, SampleApiHandler)
+        print(f'Server running at http://{SERVER_HOST}:{SERVER_PORT}')
+        httpd.serve_forever()
 
 if __name__ == '__main__':
-    run()
+    main()
+
+
+# from http.server import BaseHTTPRequestHandler, HTTPServer
+# import json
+#
+# HOST = 'localhost'
+# PORT = 8002
+#
+# class SampleApiHandler(BaseHTTPRequestHandler):
+#
+#     def _set_headers(self, status=200):
+#         self.send_response(status)
+#         self.send_header("Content-type", "application/json")
+#         self.end_headers()
+#
+#     def do_GET(self):
+#         if self.path == "/authors":
+#             self._set_headers()
+#             output = json.dumps(authors)
+#             self.wfile.write(output.encode('utf-8'))
+#         else:
+#             self._set_headers(404)
+#             self.wfile.write(json.dumps({"error": "Not found"}).encode())
+#
+#
+#     def _get_request_body(self):
+#         content_length = int(self.headers.get('Content-Length', 0))
+#         body = self.rfile.read(content_length)
+#         return json.loads(body.decode('utf-8')) if body else {}
+#
+#     def do_POST(self):
+#         if self.path == '/authors':
+#             body = self._get_request_body()
+#             author_id = len(authors) + 1
+#             author = {
+#                 'id': author_id,
+#                 'name': body.get('name', 'Unnamed authors'),
+#                 'city': body.get('city', 'unknown city')
+#             }
+#             authors.append(author)
+#             self._set_headers(201)
+#             self.wfile.write(json.dumps(author).encode())
+#         else:
+#             self._set_headers(404)
+#             self.wfile.write(b'{"error": "Not found"}')
+#
+#     def do_PUT(self):
+#         author_id = self.path.split('/')[-1]
+#         author_exists = False
+#         for author in authors:
+#             if author['id'] == int(author_id):
+#                 author_exists = True
+#                 body = self._get_request_body()
+#                 body['id'] = author_id
+#
+#                 # change in data store
+#                 author['name'] = body['name']
+#                 author['city'] = body['city']
+#                 self._set_headers(200)
+#                 self.wfile.write(json.dumps(author).encode())
+#                 break
+#
+#             if not author_exists:
+#                 self._set_headers(404)
+#                 self.wfile.write(b'{"error": "Not found"}')
+#
+#     def do_DELETE(self):
+#         author_id = self.path.split('/')[-1]
+#         author_exists = False
+#         for author in authors:
+#             if author['id'] == int(author_id):
+#                 author_exists = True
+#                 authors.remove(author)
+#                 self._set_headers(202)
+#                 break
+#         if not author_exists:
+#             self._set_headers(404)
+#             self.wfile.write(b'{"error": "Not found"}')
+#
+# def run():
+#     server_address = (HOST, PORT)
+#     httpd = HTTPServer(server_address, SampleApiHandler)
+#     print(f"Starting server on http://{HOST}:{PORT}")
+#     httpd.serve_forever()
+#
+# if __name__ == '__main__':
+#     run()
+
 
