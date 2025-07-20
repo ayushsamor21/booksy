a3bfea8a7f1c30bf094cb31fb98de4ef13eb24e9
ayush samor <ayushsamor21@gmail.com>
2025-07-20 10:03:44 +0530

    MySql connector

---
+++ restful_api/db/connect.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+class DatabaseConnector:
+    def __init__(self, host, port, database, username, password):
+        self.host = host
+        self.port = port
+        self.database = database
+        self.username = username
+        self.password = password
+
+    def connect(self):
+        db_instance = mysql.connector.connect(
+            host=self.host,
+            port=self.host,
+            database = self.database,
+            user=self.username,
+            password=self.password
+        )
+        return db_instance
